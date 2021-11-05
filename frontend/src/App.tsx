e478ccf17fa3bfa8776406db6a6a61dbcbd99136
Jocimar Walter <jocimarwalter@bb.com.br>
2021-11-05 16:26:30 -0300

    atualizado

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,12 +1,41 @@
+import BarChart from "components/Chart";
+import DataTable from "components/DataTable";
+import DonutsChart from "components/DonutsCharts";
+import Footer from "components/Footer";
+import Navbar from "components/NavBar";
 
 
-import './App.css';
+
 
 function App() {
   return (
-    <div className="App">
-      <h1>Ola Mundo</h1>
+    <>
+    <Navbar></Navbar>
+    <div  className="container">
+      
+       <h1 className="text-primary py-3">Dashbord de Vendas</h1>
+       <div className="row px-3">
+         <div className="col-sm-6">
+           <h5 className="text-center text-secondary">Taxa de sucesso(%)</h5>
+         <BarChart/>
+         </div>
+         <div className="col-sm-6">
+           <h5>Taxa de sucesso(%)</h5>
+         <DonutsChart/>
+         </div>
+       
+
+       </div>
+
+       <div className="py-3">
+         <h2 className="text-primary">Todas as vendas</h2>
+       <DataTable/>
+       </div>
+     
+      
     </div>
+    <Footer></Footer>
+    </>
   );
 }
 
