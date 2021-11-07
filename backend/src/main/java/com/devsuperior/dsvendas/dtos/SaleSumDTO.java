2bfccdc6e947116a3813bcf977bb9f25c3bd63d8
JOCIMAR <jocimarwalter@bb.com.br>
2021-11-07 18:45:16 -0300

    sd

---
+++ backend/src/main/java/com/devsuperior/dsvendas/dtos/SaleSumDTO.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.devsuperior.dsvendas.dtos;
+
+import java.io.Serializable;
+
+/**
+ *
+ * @author F5078775
+ */
+public class SaleSumDTO implements Serializable{
+    
+    private String sellerName;
+    private Double sum;
+    
+}
